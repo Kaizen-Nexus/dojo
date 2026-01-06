432d71ecc9ae2e02d6ce34bc1f23ca9c0f810d54
kyncore <yen-ning@coremobile.co.jp>
2026-01-06 20:27:37 +0900

    open 1.1-dev

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -42,26 +42,14 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->autowire()
         ->autoconfigure();
     /** release workers - in order to execute */
-    // $services->set(UpdateReplaceReleaseWorker::class);
-    // $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
-    // $services->set(AddTagToChangelogReleaseWorker::class);
-    // $services->set(TagVersionReleaseWorker::class);
-    // $services->set(PushTagReleaseWorker::class);
-    // $services->set(SetNextMutualDependenciesReleaseWorker::class);
-    // $services->set(UpdateBranchAliasReleaseWorker::class);
-    // $services->set(PushNextDevReleaseWorker::class);
-
     $services->set(UpdateReplaceReleaseWorker::class);
     $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
     $services->set(AddTagToChangelogReleaseWorker::class);
-
-    $services->set(TagVersionReleaseWorker::class);   // ✅ create tag FIRST
-    $services->set(PushTagReleaseWorker::class);      // ✅ push tag SECOND
-
-    $services->set(SetNextMutualDependenciesReleaseWorker::class);
+    $services->set(TagVersionReleaseWorker::class);
+    $services->set(PushTagReleaseWorker::class);
+    // $services->set(SetNextMutualDependenciesReleaseWorker::class);
     $services->set(UpdateBranchAliasReleaseWorker::class);
     $services->set(PushNextDevReleaseWorker::class);
-
 
 
 
